57c4e5fe31ab2ff72f9a4de31731eed23b259d0e
dhiraj19999 <dhirajgarad46@gmail.com>
2025-07-08 20:35:02 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
 import cors from "cors";
 
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/auth.js";
-
-dotenv.config();
-const app = express();
-
-// Connect to MongoDB
-connectDB();
-
-// CORS setup
 const allowedOrigins = [
-  "https://client-j05stbrie-dhiraj19999s-projects.vercel.app",
+  "https://client-j05stbrie-dhiraj19999s-projects.vercel.app"
 ];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed for this origin"));
+      }
+    },
     credentials: true,
   })
 );
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use("/api/auth", authRoutes);
-
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
 
