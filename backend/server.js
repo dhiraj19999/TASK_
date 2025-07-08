897b5262ca3b886e1ea73233be2ef63570052c3c
dhiraj19999 <dhirajgarad46@gmail.com>
2025-07-08 20:38:50 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import cors from "cors";
+
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.js";
+
+
+dotenv.config();
+
+
+const app = express();
+
+connectDB();
+
 
 const allowedOrigins = [
   "https://client-j05stbrie-dhiraj19999s-projects.vercel.app"
@@ -17,3 +32,14 @@ app.use(
   })
 );
 
+
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
